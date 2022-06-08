966697ea6c017d00f66651fee06132fdbdbc232c
chapmancheng <chapboy1989@gmail.com>
2022-06-09 05:47:01 +0800

    mv: randomDataApiResources

--- src/resources/randomDataApiResources.tsx
+++ src/resources/randomDataApiResources.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 const baseURL = "https://random-data-api.com/api/";
 const params = new URLSearchParams("?size=20");
 
-const instance = axios.create({ baseURL, params });
+const randomDataApiResources = axios.create({ baseURL, params });
 
-export default instance;
+export default randomDataApiResources;
